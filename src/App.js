1364b0333ce3a096dfe73a984b5f50558ee53ccb
Arvind M <arvind.nexus06@gmail.com>
2022-03-28 16:04:47 +0530

    addition of lazy loading component in main app js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+// import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const SignUp = lazy(() => import("./pages/SignUp"));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/login" component={Login} exact />
-        <Route path="/register" component={SignUp} exact />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/login" component={Login} exact />
+            <Route path="/register" component={SignUp} exact />
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
+    </>
   );
 }
 
