508d643f95cf1f4a9ec1972177bfeb64ee398524
Arvind M <arvind.nexus06@gmail.com>
2022-03-28 16:13:05 +0530

    minor banner image ui fix

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ const SignUp = lazy(() => import("./pages/SignUp"));
 function App() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/login" component={Login} exact />
             <Route path="/register" component={SignUp} exact />
           </Switch>
-        </BrowserRouter>
-      </Suspense>
+        </Suspense>
+      </BrowserRouter>
     </>
   );
 }
